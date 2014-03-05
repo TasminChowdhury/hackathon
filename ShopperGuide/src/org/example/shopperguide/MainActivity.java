b35402ad8f22db1e9b3a3bd7589672c2ddc9c74c
riya <tasmin.buet@gmail.com>
2014-03-05 21:08:48 +0600

    updated

--- ShopperGuide/src/org/example/shopperguide/MainActivity.java
+++ ShopperGuide/src/org/example/shopperguide/MainActivity.java
@@ -3,30 +3,60 @@ package org.example.shopperguide;
 import android.os.Bundle;
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Color;
 import android.util.Log;
 import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends Activity implements OnClickListener {
-	Button bAddShop;
-	Button bSearch;
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.main);
-		
-		bAddShop=(Button)findViewById(R.id.AddShop);
-		bSearch=(Button)findViewById(R.id.Search);
-		bAddShop.setOnClickListener((OnClickListener) this);
-		bSearch.setOnClickListener((OnClickListener) this);
+	   private EditText  username=null;
+	   private EditText  password=null;
+	   private TextView attempts;
+	   private Button login;
+	   int counter = 3;
+	   @Override
+	   protected void onCreate(Bundle savedInstanceState) {
+	      super.onCreate(savedInstanceState);
+	      setContentView(R.layout.main);
+	      username = (EditText)findViewById(R.id.etuser);
+	      password = (EditText)findViewById(R.id.etpass);
+	      attempts = (TextView)findViewById(R.id.textView5);
+	      attempts.setText(Integer.toString(counter));
+	      login = (Button)findViewById(R.id.button1);
+	   }
 
-		
-		
+	   public void login(View view){
+	      if(username.getText().toString().equals("admin") && 
+	      password.getText().toString().equals("admin")){
+	      Toast.makeText(getApplicationContext(), "Redirecting...", 
+	      Toast.LENGTH_SHORT).show();
+	   }	
+	   else{
+	      Toast.makeText(getApplicationContext(), "Wrong Credentials",
+	      Toast.LENGTH_SHORT).show();
+	      attempts.setBackgroundColor(Color.RED);	
+	      counter--;
+	      attempts.setText(Integer.toString(counter));
+	      if(counter==0){
+	         login.setEnabled(false);
+	      }
+
+	   }
+
 	}
+	 
+
 	
+		
+		
+
+
 	
 
 	@Override
@@ -37,22 +67,12 @@ public class MainActivity extends Activity implements OnClickListener {
 	}
 
 	@Override
-	public void onClick(View v) {
+	public void onClick(View arg0) {
 		// TODO Auto-generated method stub
-		int id = v.getId();
-	    if (id == R.id.AddShop){
-	        
-	    } 
-	    else if (id == R.id.Search){
-	    	 Log.i("clicks","You Cljkfhhccbuysdicked B1");
-             Intent i=new Intent(MainActivity.this,Search.class);
-             startActivity(i);
-           }
-
-	        
-	        
-	    } 
 		
-	
+	}
 
-}
+	        
+	        
+} 
+		
--- ShopperGuide/src/org/example/shopperguide/Search.java
+++ ShopperGuide/src/org/example/shopperguide/Search.java
@@ -1,14 +1,24 @@
 package org.example.shopperguide;
 
+import com.google.android.maps.MapActivity;
+import com.google.android.maps.MapView;
+
 import android.app.Activity;
 import android.os.Bundle;
 
-public class Search extends Activity {
+public class Search extends MapActivity {
+	MapView map;
 	 @Override
 	    protected void onCreate(Bundle savedInstanceState) {
 	        super.onCreate(savedInstanceState);
 	        setContentView(R.layout.search);
 	    }
+
+	@Override
+	protected boolean isRouteDisplayed() {
+		// TODO Auto-generated method stub
+		return false;
+	}
 
 	  /*  @Override
 	    public boolean onCreateOptionsMenu(Menu menu) {
