7660fdf049f6c796e222f3a97b9740f869e50846
nafi <anzumnafisa13@gmail.com>
2014-03-06 22:38:02 +0600

    icon added

--- ShopperGuide/gen/org/example/shopperguide/R.java
+++ ShopperGuide/gen/org/example/shopperguide/R.java
@@ -40,7 +40,6 @@ public final class R {
         public static final int main=0x7f030000;
         public static final int search=0x7f030001;
         public static final int shoppage=0x7f030002;
-        public static final int spinner=0x7f030003;
     }
     public static final class menu {
         public static final int main=0x7f070000;
