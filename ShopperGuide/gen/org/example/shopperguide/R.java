4d097f207633e1316530944c0ae667e31b3fd805
nafi <anzumnafisa13@gmail.com>
2014-03-07 09:26:19 +0600

    Start page changed

--- ShopperGuide/gen/org/example/shopperguide/R.java
+++ ShopperGuide/gen/org/example/shopperguide/R.java
@@ -30,7 +30,8 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
         public static final int ic_launcher1=0x7f020001;
         public static final int search=0x7f020002;
-        public static final int starting=0x7f020003;
+        public static final int start=0x7f020003;
+        public static final int starting=0x7f020004;
     }
     public static final class id {
         public static final int MyShopPage=0x7f0a0010;
